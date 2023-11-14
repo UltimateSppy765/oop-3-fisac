791dfaaa7da1dfbc465571dcf9b1fca86fc4462e
UltimateSppy765 <73820738+UltimateSppy765@users.noreply.github.com>
2023-11-14 16:45:33 +0530

    Remove unnecessary code

--- src/screens/LoginPanel.java
+++ src/screens/LoginPanel.java
@@ -15,40 +15,30 @@ public class LoginPanel extends JPanel {
 	 * Create the panel.
 	 */
 	public LoginPanel() {
+		super(new GridBagLayout());
 		setName("Login Panel");
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0};
-		gridBagLayout.rowHeights = new int[]{0};
-		gridBagLayout.rowWeights = new double[]{0.0};
-		gridBagLayout.columnWeights = new double[]{0.0};
-		setLayout(gridBagLayout);
 		
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setDividerSize(0);
-		GridBagConstraints gbc_splitPane = new GridBagConstraints();
-		gbc_splitPane.gridx = 0;
-		gbc_splitPane.gridy = 0;
-		add(splitPane, gbc_splitPane);
+		add(splitPane);
 		
-		JLabel imageLabel = new JLabel(new ImageIcon(LoginPanel.class.getResource("/media/SchoolRoom.jpg")));
-		splitPane.setLeftComponent(imageLabel);
+		splitPane.setLeftComponent(new JLabel(
+			new ImageIcon(
+				LoginPanel.class.getResource("/media/SchoolRoom.jpg")
+			)
+		));
 		
 		JPanel optionsPanel = new JPanel();
 		splitPane.setRightComponent(optionsPanel);
 		GridBagLayout gbl_optionsPanel = new GridBagLayout();
-		gbl_optionsPanel.columnWidths = new int[]{15, 0, 15, 0};
-		gbl_optionsPanel.rowHeights = new int[]{0, 5, 0, 10, 0, 0};
-		gbl_optionsPanel.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_optionsPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_optionsPanel.columnWidths = new int[]{15, 0, 15};
+		gbl_optionsPanel.rowHeights = new int[]{0, 0, 10, 0};
 		optionsPanel.setLayout(gbl_optionsPanel);
 		
 		JPanel usernamePanel = new JPanel();
 		GridBagConstraints gbc_usernamePanel = new GridBagConstraints();
 		gbc_usernamePanel.anchor = GridBagConstraints.SOUTH;
-		gbc_usernamePanel.insets = new Insets(0, 0, 5, 5);
-		gbc_usernamePanel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_usernamePanel.gridx = 1;
-		gbc_usernamePanel.gridy = 0;
 		optionsPanel.add(usernamePanel, gbc_usernamePanel);
 		
 		JLabel usernameLabel = new JLabel("Username");
@@ -61,27 +51,21 @@ public class LoginPanel extends JPanel {
 		
 		JPanel passwordPanel = new JPanel();
 		GridBagConstraints gbc_passwordPanel = new GridBagConstraints();
-		gbc_passwordPanel.insets = new Insets(0, 0, 5, 5);
-		gbc_passwordPanel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_passwordPanel.gridx = 1;
-		gbc_passwordPanel.gridy = 2;
 		optionsPanel.add(passwordPanel, gbc_passwordPanel);
 		
 		JLabel passwordLabel = new JLabel("Password");
 		passwordPanel.add(passwordLabel);
 		
-		passwordField = new JPasswordField();
+		passwordField = new JPasswordField(10);
 		passwordLabel.setLabelFor(passwordField);
-		passwordField.setColumns(10);
 		passwordPanel.add(passwordField);
 		
 		JPanel buttonPanel = new JPanel();
 		GridBagConstraints gbc_buttonPanel = new GridBagConstraints();
-		gbc_buttonPanel.insets = new Insets(0, 0, 0, 5);
 		gbc_buttonPanel.anchor = GridBagConstraints.NORTH;
-		gbc_buttonPanel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_buttonPanel.gridx = 1;
-		gbc_buttonPanel.gridy = 4;
+		gbc_buttonPanel.gridy = 3; // Row 2 becomes vertical gap
 		optionsPanel.add(buttonPanel, gbc_buttonPanel);
 		
 		JButton loginButton = new JButton("Login");
@@ -93,8 +77,6 @@ public class LoginPanel extends JPanel {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
-		});
-		buttonPanel.add(exitButton);
+		}); buttonPanel.add(exitButton);
 	}
-
 }
